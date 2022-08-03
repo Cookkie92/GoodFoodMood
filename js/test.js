5f12ca145ff3d1b6e9ec2338de1caf1f8aeff38e
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-08-04 00:07:45 +0200

    update

--- js/test.js
+++ js/test.js
@@ -3,20 +3,20 @@ const options = {
   method: "GET",
   headers: {
     "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
-    "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
   },
 };
 
 function searchRecipe(query) {
-  const url = `https://edamam-recipe-search.p.rapidapi.com/search?q=${query}`;
+  const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${query}&offset=0&number=20&addRecipeInformation=true&instructionsRequired=true`;
 
   fetch(url, options)
     .then((response) => response.json())
 
     .then((jsonData) => {
       try {
-        // const results = jsonData.hits.map((element) => element.recipe.label);
-        // renderResults(results);
+        const results = jsonData.results.map((element) => element.title);
+        renderResults(results);
         console.log(jsonData);
       } catch (error) {
         console.log(error);
