1ecf6c578e2e284ab79cb38ab5e2a93476ffdc01
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-08-07 17:54:56 +0200

    update

--- js/details.js
+++ js/details.js
@@ -1,25 +1,25 @@
 const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
-      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    },
-  };
-  const recipeResult = document.getElementById("recipeResult")
-  fetch(
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=veryHealthy%2Cdinner&number=50",
-    options
-  )
-    .then((response) => response.json())
-  
-    .then((jsonData) => {
-      try {
-        const getRecipes = jsonData;
-        console.log(getRecipes)
-        recipeResult.innerHTML = "";
-        for (let i = 0; i < getRecipes.recipes.length; i++) {
-          console.log(i)
-          recipeResult.innerHTML += `
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  },
+};
+const recipeResult = document.getElementById("recipeResult");
+fetch(
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=veryHealthy%2Cdinner&number=50",
+  options
+)
+  .then((response) => response.json())
+
+  .then((jsonData) => {
+    try {
+      const getRecipes = jsonData;
+      console.log(getRecipes);
+      recipeResult.innerHTML = "";
+      for (let i = 0; i < getRecipes.recipes.length; i++) {
+        console.log(i);
+        recipeResult.innerHTML += `
   
           <div class="recipe-results">
           <a href="details.html?id=${getRecipes.recipes[i].id}">
@@ -33,13 +33,9 @@ const options = {
              </a>
           
           </div>
-          `
-        }
-  
-        
-        
-      } catch (error) {
-        console.log(error);
+          `;
       }
-    });
-  
+    } catch (error) {
+      console.log(error);
+    }
+  });
--- js/recipes.js
+++ js/recipes.js
@@ -5,7 +5,7 @@ const options = {
     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
   },
 };
-const recipeResult = document.getElementById("recipeResult")
+const recipeResult = document.getElementById("recipeResult");
 fetch(
   "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=veryHealthy%2Cdinner&number=50",
   options
@@ -15,10 +15,10 @@ fetch(
   .then((jsonData) => {
     try {
       const getRecipes = jsonData;
-      console.log(getRecipes)
+      console.log(getRecipes);
       recipeResult.innerHTML = "";
       for (let i = 0; i < getRecipes.recipes.length; i++) {
-        console.log(i)
+        console.log(i);
         recipeResult.innerHTML += `
 
         <div class="recipe-results">
@@ -33,11 +33,8 @@ fetch(
            </a>
         
         </div>
-        `
+        `;
       }
-
-      
-      
     } catch (error) {
       console.log(error);
     }
