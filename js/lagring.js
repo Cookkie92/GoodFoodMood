f74372bb160aaf1cd976e0ec901c02c1a78b7398
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-08-15 16:59:15 +0200

    update

--- js/lagring.js
+++ js/lagring.js
@@ -1,52 +1,52 @@
-const resultContainer = document.querySelector(".inner-highlights");
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
-    "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
-  },
-};
+// const resultContainer = document.querySelector(".inner-highlights");
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
+//     "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+//   },
+// };
 
-function searchRecipe(query) {
-  const url = `https://edamam-recipe-search.p.rapidapi.com/search?q=${query}`;
+// function searchRecipe(query) {
+//   const url = `https://edamam-recipe-search.p.rapidapi.com/search?q=${query}`;
 
-  fetch(url, options)
-    .then((response) => response.json())
+//   fetch(url, options)
+//     .then((response) => response.json())
 
-    .then((jsonData) => {
-      try {
-        // const results = jsonData.hits.map((element) => element.recipe.label);
-        // renderResults(results);
-        console.log(jsonData);
-      } catch (error) {
-        console.log(error);
-      }
-    });
-}
-searchRecipe();
+//     .then((jsonData) => {
+//       try {
+//         // const results = jsonData.hits.map((element) => element.recipe.label);
+//         // renderResults(results);
+//         console.log(jsonData);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     });
+// }
+// searchRecipe();
 
-function renderResults(results) {
-  const list = document.getElementById("resultslist");
-  list.innerHTML = "";
-  results.forEach((result) => {
-    const element = document.createElement("li");
-    element.innerText = result;
-    list.appendChild(element);
-    console.log(result);
-  });
-}
-let searchTimeoutToken = 0;
-window.onload = () => {
-  const searchFieldElement = document.getElementById("searchField");
-  searchFieldElement.onkeyup = (event) => {
-    clearTimeout(searchTimeoutToken);
+// function renderResults(results) {
+//   const list = document.getElementById("resultslist");
+//   list.innerHTML = "";
+//   results.forEach((result) => {
+//     const element = document.createElement("li");
+//     element.innerText = result;
+//     list.appendChild(element);
+//     console.log(result);
+//   });
+// }
+// let searchTimeoutToken = 0;
+// window.onload = () => {
+//   const searchFieldElement = document.getElementById("searchField");
+//   searchFieldElement.onkeyup = (event) => {
+//     clearTimeout(searchTimeoutToken);
 
-    if (searchFieldElement.value.trim().length === 0) {
-      return;
-    }
+//     if (searchFieldElement.value.trim().length === 0) {
+//       return;
+//     }
 
-    searchTimeoutToken = setTimeout(() => {
-      searchRecipe(searchFieldElement.value);
-    }, 250);
-  };
-};
+//     searchTimeoutToken = setTimeout(() => {
+//       searchRecipe(searchFieldElement.value);
+//     }, 250);
+//   };
+// };
--- js/recipes.js
+++ js/recipes.js
@@ -1,43 +1,43 @@
-let options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
-    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  },
-};
+// let options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
+//     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+//   },
+// };
 
-const recipeResult = document.getElementById("recipeResult");
-fetch(
-  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=veryHealthy%2Cdinner&number=50",
-  options
-)
-  .then((response) => response.json())
+// const recipeResult = document.getElementById("recipeResult");
+// fetch(
+//   "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=veryHealthy%2Cdinner&number=50",
+//   options
+// )
+//   .then((response) => response.json())
 
-  .then((jsonData) => {
-    try {
-      const getRecipes = jsonData;
-      console.log(getRecipes);
-      recipeResult.innerHTML = "";
-      for (let i = 0; i < getRecipes.recipes.length; i++) {
-        if (getRecipes.recipes[i].image === 0) {
-          image.style.display = "none";
-        }
-        console.log(i);
-        recipeResult.innerHTML += `
+//   .then((jsonData) => {
+//     try {
+//       const getRecipes = jsonData;
+//       console.log(getRecipes);
+//       recipeResult.innerHTML = "";
+//       for (let i = 0; i < getRecipes.recipes.length; i++) {
+//         if (getRecipes.recipes[i].image === 0) {
+//           image.style.display = "none";
+//         }
+//         console.log(i);
+//         recipeResult.innerHTML += `
 
-        <div class="recipe-results">
-        <a href="details.html?id=${getRecipes.recipes[i].id} ">
-        
-        <div class = "inner-result">
-        <img class="image" src="${getRecipes.recipes[i].image}"  alt="thumbnail">      
-        <h3 class="recipe-logo">${getRecipes.recipes[i].title}</h3>
-           </div>
-           </a>
+//         <div class="recipe-results">
+//         <a href="details.html?id=${getRecipes.recipes[i].id} ">
 
-        </div>
-        `;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  });
+//         <div class = "inner-result">
+//         <img class="image" src="${getRecipes.recipes[i].image}"  alt="thumbnail">
+//         <h3 class="recipe-logo">${getRecipes.recipes[i].title}</h3>
+//            </div>
+//            </a>
+
+//         </div>
+//         `;
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
