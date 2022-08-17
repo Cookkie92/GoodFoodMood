4cae06a5eb2be282b591fbcf15ed05d749daefae
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-08-17 20:19:24 +0200

    update

--- js/contact.js
+++ js/contact.js
@@ -5,34 +5,44 @@ const contactSubject = document.querySelector("#subject");
 const subjectError = document.querySelector("#subjectError");
 const contactEmail = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
-const contactAdress = document.querySelector("#adress");
-const adressError = document.querySelector("#adressError");
 const contactSucess = document.querySelector("#sucess");
 
 function validateForm() {
   try {
     event.preventDefault();
 
+    let nameSuccessful = false;
+    let subjectSuccessful = false;
+    let emailSuccessful = false;
+
     if (checkLength(contactName.value, 0) === true) {
       nameError.style.display = "none";
+      nameSuccessful = true;
     } else {
       nameError.style.display = "block";
+      nameSuccessful = false;
     }
     if (checkLength(contactSubject.value, 9) === true) {
       subjectError.style.display = "none";
+      subjectSuccessful = true;
     } else {
       subjectError.style.display = "block";
+      subjectSuccessful = false;
     }
-    if (checkLength(contactAdress.value, 24) === true) {
-      adressError.style.display = "none";
-    } else {
-      adressError.style.display = "block";
-    }
-
     if (validateEmail(email.value) === true) {
       emailError.style.display = "none";
+      emailSuccessful = true;
     } else {
       emailError.style.display = "block";
+      emailSuccessful = false;
+    }
+    if (
+      nameSuccessful === true &&
+      subjectSuccessful === true &&
+      emailSuccessful === true
+    ) {
+      console.log("show me the way");
+      document.getElementById("sucess").style.display = "block";
     }
   } catch (error) {
     console.log("Error occured", error);
@@ -54,47 +64,3 @@ function validateEmail(email) {
   const patternMatches = regEx.test(email);
   return patternMatches;
 }
-
-// console.log();
-
-//lvl2 attempt
-// contactSubject.len,
-// contactAdress.len,
-// email.value
-// if (checkLength(value, len) || validateEmail(emails) === true) {
-//   contactSucess.style.display = "none";
-// } else {
-//   contactSucess.style.display = "block";
-// }
-// if(validateForm(value,len)) === true{
-//   contactSucess.style.display = "none";
-// } else{
-//   contactSucess.style.display = "block";
-// }
-
-// if (
-//   checkLength(
-//     contactName.value,
-//     contactSubject.value,
-//     contactAdress.value,
-//     len
-//   ) === true
-// ) {
-//   contactSucess.style.display = "none";
-// } else {
-//   contactSucess.style.display = "block";
-// }
-
-// if (
-//   validateForm(
-//     contactName.len,
-//     contactSubject.len,
-//     contactAdress.len,
-//     email.value
-//   ) === true
-// ) {
-//   contactSucess.style.display = "none";
-// } else {
-//   contactSucess.style.display = "block";
-// }
-//NOPE
