7d240800e238b2595e28e932b52e205f90e30274
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-08-18 12:57:47 +0200

    push it yes harder

--- js/details.js
+++ js/details.js
@@ -1,39 +1,68 @@
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
-//     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-//   },
-// };
-// const recipeResult = document.getElementById("recipeResult");
-// fetch(
-//   "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=veryHealthy%2Cdinner&number=50",
-//   options
-// )
-//   .then((response) => response.json())
+const detailsContainer = document.querySelector(".recipe-details");
+const queryString = document.location.search;
 
-//   .then((jsonData) => {
-//     try {
-//       const getRecipes = jsonData;
-//       console.log(getRecipes);
-//       recipeResult.innerHTML = "";
-//       for (let i = 0; i < getRecipes.recipes.length; i++) {
-//         console.log(i);
-//         recipeResult.innerHTML += `
+const params = new URLSearchParams(queryString);
 
-//           <div class="recipe-results">
-//           <a href="details.html?id=${getRecipes.recipes[i].id}">
-//           <div class = "inner-result">
-//           <h3 class="logo">${getRecipes.recipes[i].title}</h3>
-//             <img class="image" src="${getRecipes.recipes[i].image}"  alt="thumbnail">
+const id = params.get("id");
 
-//              </div>
-//              </a>
+console.log(id);
+console.log("hallo");
 
-//           </div>
-//           `;
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  },
+};
+
+fetch(
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" +
+    id +
+    "/information?",
+  options
+)
+  .then((response) => response.json())
+
+  .then((data) => {
+    try {
+      detailsContainer.innerHTML = "";
+      const recipes = data;
+
+      let ingred = getSteps(recipes);
+      const detailsTitle = document.querySelector("title");
+      detailsTitle.innerHTML = `${recipes.title}`;
+      detailsContainer.innerHTML += `
+      <div class="recipe-details>
+      <div class="inner-details">
+            <h1>${recipes.title}</h1>
+            ${recipes.summary}
+            <img class="details-image" src="${recipes.image}" alt="very nice food"
+            Diets:<p>${recipes.diets}</p>
+            ${ingred}
+            <p>${recipes.instructions}</p>
+
+
+        </div>
+      </div>
+      `;
+
+      console.log(recipes);
+    } catch (error) {
+      console.warn("Something Went Wrong", error);
+      detailsContainer.innerHTML += `
+      <h3 class="logo">Something Went Wrong!</h3>
+      `;
+    }
+  });
+
+function getSteps(recipes) {
+  let extendedingrediants = "";
+  for (let i = 0; i < recipes.extendedIngredients.length; i++) {
+    extendedingrediants +=
+      "<p>" + recipes.extendedIngredients[i].original + "</p>";
+    console.log(i);
+  }
+
+  return extendedingrediants;
+}
