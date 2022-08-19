0832d5b87cebe7e6a326cfb85cee747a4d623399
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-08-19 13:35:06 +0200

    update

---
+++ js/featured.js
@@ -0,0 +1,43 @@
+let options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  },
+};
+
+const recipeResult = document.getElementById("featuredResult");
+fetch(
+  "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=veryHealthy%2Cdinner&number=3",
+  options
+)
+  .then((response) => response.json())
+
+  .then((jsonData) => {
+    try {
+      const getRecipes = jsonData;
+      console.log(getRecipes);
+      recipeResult.innerHTML = "";
+      for (let i = 0; i < getRecipes.recipes.length; i++) {
+        if (typeof getRecipes.recipes[i].image === "undefined") {
+          continue;
+        }
+        console.log(i);
+        recipeResult.innerHTML += `
+
+        <div class="featured-result">
+        <a href="details.html?id=${getRecipes.recipes[i].id} ">
+
+        <div class = "inner-result">
+        <img class="image" src="${getRecipes.recipes[i].image}"  alt="thumbnail">
+        <h3 class="recipe-logo">${getRecipes.recipes[i].title}</h3>
+           </div>
+           </a>
+
+        </div>
+        `;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
--- js/search.js
+++ js/search.js
@@ -1,79 +1,79 @@
-// const resultsList = document.getElementById("#resultslist");
-// const searchingForm = document.querySelector("#searchForm");
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
-//     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-//   },
-// };
-// function searchRecipe() {
-//   let url = createUrl();
-//   console.log(url);
-//   fetch(url, options)
-//     .then((response) => response.json())
+const resultsList = document.getElementById("#resultslist");
+const searchingForm = document.querySelector("#searchForm");
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3716894aaemshae6e469d90fcb52p1fa571jsn8c269b788f98",
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  },
+};
+function searchRecipe() {
+  let url = createUrl();
+  console.log(url);
+  fetch(url, options)
+    .then((response) => response.json())
 
-//     .then((jsonData) => {
-//       try {
-//         let results = jsonData.results;
-//         renderResults(results);
+    .then((jsonData) => {
+      try {
+        let results = jsonData.results;
+        renderResults(results);
 
-//         console.log(jsonData);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     });
-// }
-// // searchRecipe();
+        console.log(jsonData);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+}
+// searchRecipe();
 
-// function renderResults(results) {
-//   const recipeResult = document.getElementById("resultslist");
-//   // const getRecipes = jsonData;
-//   // console.log(getRecipes);
-//   recipeResult.innerHTML = "";
-//   for (let i = 0; i < results.length; i++) {
-//     if (typeof results[i].image === "undefined") {
-//       continue;
-//     }
-//     console.log(i);
-//     recipeResult.innerHTML += `
+function renderResults(results) {
+  const recipeResult = document.getElementById("resultslist");
+  // const getRecipes = jsonData;
+  // console.log(getRecipes);
+  recipeResult.innerHTML = "";
+  for (let i = 0; i < results.length; i++) {
+    if (typeof results[i].image === "undefined") {
+      continue;
+    }
+    console.log(i);
+    recipeResult.innerHTML += `
 
-//         <div class="recipe-results">
-//         <a href="details.html?id=${results[i].id} ">
+        <div class="recipe-results">
+        <a href="details.html?id=${results[i].id} ">
 
-//         <div class = "inner-result">
-//         <img class="image" src="${results[i].image}"  alt="thumbnail">
-//         <h3 class="recipe-logo">${results[i].title}</h3>
-//            </div>
-//            </a>
+        <div class = "inner-result">
+        <img class="image" src="${results[i].image}"  alt="thumbnail">
+        <h3 class="recipe-logo">${results[i].title}</h3>
+           </div>
+           </a>
 
-//         </div>
-//         `;
-//   }
-// }
+        </div>
+        `;
+  }
+}
 
-// function createUrl() {
-//   const baseUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?number=50&addRecipeInformation=true&instructionsRequired=true&`;
-//   let url = baseUrl;
-//   let recipeInput = document.getElementsByClassName("recipe-input");
-//   let recipeInputArray = [...recipeInput];
-//   let urlHeaders = "";
+function createUrl() {
+  const baseUrl = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?number=50&addRecipeInformation=true&instructionsRequired=true&`;
+  let url = baseUrl;
+  let recipeInput = document.getElementsByClassName("recipe-input");
+  let recipeInputArray = [...recipeInput];
+  let urlHeaders = "";
 
-//   recipeInputArray.forEach((headers) => {
-//     // headers.name;
-//     if (headers.name === "query" && headers.value === null) {
-//       return;
-//     }
+  recipeInputArray.forEach((headers) => {
+    // headers.name;
+    if (headers.name === "query" && headers.value === null) {
+      return;
+    }
 
-//     if (headers.value === null || headers.value === "") {
-//       return;
-//     }
+    if (headers.value === null || headers.value === "") {
+      return;
+    }
 
-//     urlHeaders = urlHeaders + headers.name + "=" + headers.value + "&";
-//   });
+    urlHeaders = urlHeaders + headers.name + "=" + headers.value + "&";
+  });
 
-//   url += urlHeaders;
+  url += urlHeaders;
 
-//   return url;
-// }
-// createUrl();
+  return url;
+}
+createUrl();
